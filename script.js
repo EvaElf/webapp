5b152db644e139c679738abc1b02410f69773308
Sofie <44964284+1074193@users.noreply.github.com>
2019-02-20 12:18:35 +0100

    funktionalitet på startside knapper

--- script.js
+++ script.js
@@ -27,15 +27,49 @@
 
 
 function showPage(page) {
-    let forside = document.getElementById('forside');
-    let madoversigt = document.getElementById('madoversigt');
-    let opskrifter = document.getElementById('opskrifter');
-    let profil = document.getElementById('profil');
+    // let forside = document.getElementById('forside');
+    // let madoversigt = document.getElementById('madoversigt');
+    // let opskrifter = document.getElementById('opskrifter');
+    // let profil = document.getElementById('profil');
 
     let all_pages = document.getElementsByClassName('nav_item');
 
     for(let i = 0; i < all_pages.length; i++) {
-       all_pages[i].style.backgroundColor = '#7A030D';
+        all_pages[i].style.backgroundColor = '#7A030D';
+    }
+
+    if (page.id === 'opret') {
+        forside.style.display = 'none';
+        madoversigt.style.display = 'none';
+        opskrifter.style.display = 'none';
+        profil.style.display = 'none';
+        startside.style.display = 'none';
+        opretbruger.style.display = 'block';
+        logind.style.display = 'none';
+    }
+
+    if (page.id === 'log') {
+        forside.style.display = 'none';
+        madoversigt.style.display = 'none';
+        opskrifter.style.display = 'none';
+        profil.style.display = 'none';
+        startside.style.display = 'none';
+        opretbruger.style.display = 'none';
+        logind.style.display = 'block';
+    }
+
+    if (page.id === 'spring') {
+        forside.style.display = 'block';
+        madoversigt.style.display = 'none';
+        opskrifter.style.display = 'none';
+        profil.style.display = 'none';
+        startside.style.display = 'none';
+        opretbruger.style.display = 'none';
+        logind.style.display = 'none';
+
+        topbar.style.display = 'block';
+        nav.style.display = 'block';
+        nav_forside.style.backgroundColor = '#BA0816';
     }
 
     if (page.id === 'nav_forside') {
@@ -47,6 +81,8 @@ function showPage(page) {
         opretbruger.style.display = 'none';
         logind.style.display = 'none';
 
+        topbar.style.display = 'block';
+        nav.style.display = 'block';
         nav_forside.style.backgroundColor = '#BA0816';
     }
 
